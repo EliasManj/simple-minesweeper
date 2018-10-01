a31943245db20beee4296e8be5cb626cca4db905
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-10-01 11:34:17 -0500

    count function

--- Board.py
+++ Board.py
@@ -14,8 +14,11 @@ class Cell:
         self.y = y
         self.flagged = False
 
+    def is_bomb(self):
+        return self.value == Cell.BOMB_VALUE
+
     def print_cell_debug(self):
-        return self.value
+        return str(self.value)
 
 class Board:
 
@@ -31,11 +34,20 @@ class Board:
         self.board = [Cell.BOMB_VALUE]*self.n_bombs + [0]*self.n_valid_cells
         shuffle(self.board)
         self.board = [self.board[i:i+self.n_cols] for i in range(0, self.size, self.n_rows)]
-        for row in self.board:
-            for col in self.board:
+        for row in range(0,self.n_rows):
+            for col in range(0,self.n_cols):
                 self.board[row][col] = Cell(value=self.board[row][col], x=row, y=col)
         self.print_board_debug()
 
+    def fill_cells(self):
+        for row in range(0,self.n_rows):
+            for col in range(0,self.n_cols):
+                if not self.board[row][col].is_bomb(): 
+                    self.count_bombs(self.board[row][col])
+
+    def count_bombs(self, cell):
+        assert cell.value != Cell.BOMB_VALUE
+        
     def print_board_debug(self):
         for row in self.board:
             print(list(map(lambda x: x.print_cell_debug(), row)))
